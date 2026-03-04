edddb33aa5f1d603acfdb6a6203d7ddb9ac59677
Tim-Kenya <171836577+Tim-Kenya@users.noreply.github.com>
2026-03-04 14:50:02 +0300

    feat(movie-info): add POST /movie-info/save endpoint to persist movie data via repository

---
+++ movie-catalog-service/src/main/java/com/video_streaming_app/movie_catalog_service/controller/MovieInfoController.java
@@ -0,0 +1,21 @@
+package com.video_streaming_app.movie_catalog_service.controller;
+
+import com.video_streaming_app.movie_catalog_service.model.MovieInfo;
+import com.video_streaming_app.movie_catalog_service.model.MovieInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieInfoController {
+    @Autowired
+    private MovieInfoRepository repository;
+
+    @PostMapping("/movie-info/save")
+    public List<MovieInfo> saveAll(@RequestBody List<MovieInfo> movieInfoList){
+        return repository.saveAll(movieInfoList);
+    }
+}
