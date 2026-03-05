7aa1fefecad7d1cec20d5facd3fcb7478e069a98
Tim-Kenya <171836577+Tim-Kenya@users.noreply.github.com>
2026-03-05 14:33:53 +0300

    feat(movie-streaming): Add movie streaming service

--- movie-catalog-service/src/main/java/com/video_streaming_app/movie_catalog_service/controller/MovieInfoController.java
+++ movie-catalog-service/src/main/java/com/video_streaming_app/movie_catalog_service/controller/MovieInfoController.java
@@ -3,6 +3,7 @@ package com.video_streaming_app.movie_catalog_service.controller;
 import com.video_streaming_app.movie_catalog_service.model.MovieInfo;
 import com.video_streaming_app.movie_catalog_service.model.MovieInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class MovieInfoController {
     @PostMapping("/movie-info/save")
     public List<MovieInfo> saveAll(@RequestBody List<MovieInfo> movieInfoList){
         return repository.saveAll(movieInfoList);
+    }
+
+    @GetMapping("/movie-info/list")
+    public List<MovieInfo> getAll(){
+        return repository.findAll();
     }
 }
--- movie-catalog-service/src/main/java/com/video_streaming_app/movie_catalog_service/model/MovieInfoRepository.java
+++ movie-catalog-service/src/main/java/com/video_streaming_app/movie_catalog_service/model/MovieInfoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MovieInfoRepository extends JpaRepository<MovieInfo, long> {
+public interface MovieInfoRepository extends JpaRepository<MovieInfo, Long> {
 
 }
